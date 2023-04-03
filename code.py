9c15d728241b3c26fa20d500f558a8a4e7122134
jaisygeorge <127745878+jaisygeorge@users.noreply.github.com>
2023-04-03 12:51:55 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from tensorflow.keras.models import load_model
+
+# load pre-trained model
+model = load_model('model_Mar29_2')
+
+# load the scaler and pca objects from disk
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+with open('pca.pkl', 'rb') as f:
+    pca = pickle.load(f)
+    
+print("Hello World")
+    
