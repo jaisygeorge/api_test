a6570979039ed0278931c3bddd9dbddf738ff550
jaisygeorge <127745878+jaisygeorge@users.noreply.github.com>
2023-04-03 11:36:27 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-get_dict = {'is_favorite' : 1,
-            'preference'  : 82,
-            'total_visit' : 25,
-            'last_visit'  : '2023-02-14T17:44:32.490Z',
-            'distance'    : 19}
-print(get_dict)
